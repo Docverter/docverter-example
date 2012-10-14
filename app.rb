af5785a5e1414bf007fc6bc10b887cb859e17568
Pete Keen <peter.keen@bugsplat.info>
2012-10-13 22:01:10 -0700

    Make it actually work

--- app.rb
+++ app.rb
@@ -2,18 +2,26 @@
 require 'rubygems'
 require 'sinatra/base'
 require 'docverter'
-require 'net/http'
+require 'net/https'
 require 'uri'
 
 class App < Sinatra::Base
+
+  Docverter.api_key = ENV['DOCVERTER_API_KEY']
 
   get '/' do
     erb :index
   end
 
-  get '/:page_name.pdf' do
-    uri = URI("http://raw.github.com/#{ENV['GITHUB_REPO']}/#{params[:page_name]}.md")
-    content = Net::HTTP.get(uri)
+  get %r{/(.*).pdf} do |path|
+    uri = URI("https://raw.github.com/#{ENV['GITHUB_REPO']}/master/#{path}.md")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    content = response.body
+
     res = Docverter::Conversion.run do |c|
       c.from    = "markdown"
       c.to      = "pdf"
